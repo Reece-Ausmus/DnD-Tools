00676ec4f141054c6dca6fa9af154b942e34ff8c
Reece Ausmus <reece@ausmusfamily.com>
2025-06-10 17:06:16 -0400

    can make a white square grid

--- frontend/src/pages/Map.tsx
+++ frontend/src/pages/Map.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, Grid } from "@mui/material";
+
+const GRID_SIZE = 10; // Adjust this value as needed
 
 const Map: React.FC = () => {
   return (
@@ -7,14 +9,30 @@ const Map: React.FC = () => {
       <Typography variant="h1" align="center" sx={{ marginTop: "20px" }}>
         Map Page
       </Typography>
-      <Box
-        component="img"
-        src="../images/hehe.jpeg"
-        sx={{
-          width: "300px",
-          height: "auto",
-        }}
-      />
+
+      {[...Array(GRID_SIZE)].map((_, rowIndex) => (
+        <Grid
+          container
+          key={rowIndex}
+          spacing={1}
+          justifyContent="center"
+          wrap="nowrap"
+          sx={{ marginTop: 1 }}
+        >
+          {[...Array(GRID_SIZE)].map((_: unknown, colIndex: number) => (
+            <Grid key={colIndex}>
+              <Box
+                sx={{
+                  width: 50,
+                  height: 50,
+                  border: "1px solid black",
+                  backgroundColor: "#f0f0f0",
+                }}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      ))}
     </Container>
   );
 };
