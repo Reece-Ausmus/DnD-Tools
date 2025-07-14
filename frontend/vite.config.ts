b28307b8857ee1e83c8780ea46399dcb6ce36537
Graham Asam <grahamasam@gmail.com>
2025-07-14 16:43:18 -0400

    experiment dev docker configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,31 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  server: {
+    host: true,
+    port: 5173,
+    hmr: {
+      protocol: "ws",
+      host: "localhost",
+    },
+    watch: {
+      usePolling: true,
+    },
+    proxy: {
+      // This rule will now handle both regular HTTP requests AND WebSockets
+      "/api": {
+        target: "http://backend:5000", // Use your backend's port
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        ws: true,
+      },
+
+      "/socket.io": {
+        target: "http://backend:5000",
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+  },
 });
